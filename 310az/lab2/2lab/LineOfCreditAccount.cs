1ab9c038462082121aee10dfc2e1a45a152db398
AnkudK <ankud.kirill56@gmail.com>
2024-06-06 19:19:00 +0700

    Add files via upload

---
+++ 310az/lab2/2lab/LineOfCreditAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOProgramming{
+
+class LineOfCreditAccount : BankAccount
+{
+    // <ConstructLineOfCredit>
+    public LineOfCreditAccount(string name, decimal initialBalance, decimal creditLimit) : base(name, initialBalance, -creditLimit)
+    {
+    }
+    // </ConstructLineOfCredit>
+
+    // <ApplyMonthendInterest>
+    public override void PerformMonthEndTransactions()
+    {
+        if (Balance < 0)
+        {
+            // Уменьшить баланс чтобы получить начисления
+            decimal interest = -Balance * 0.07m;
+            MakeWithdrawal(interest, DateTime.Now, "Charge monthly interest");
+        }
+    }
+    // </ApplyMonthendInterest>
+
+    // <AddOverdraftFee>
+    protected override Transaction? CheckWithdrawalLimit(bool isOverdrawn) =>
+        isOverdrawn
+        ? new Transaction(-20, DateTime.Now, "Apply overdraft fee")
+        : default;
+    // </AddOverdraftFee>
+}
+}
